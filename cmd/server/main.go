a679578f2cfb577f01f0fb1d4cfa4b77ff4fdf1a
ilia.lavrinenko <kivadratik@yandex.ru>
2022-01-24 18:40:01 +0300

    Server refactoring

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -32,7 +32,7 @@ type metric struct {
 var (
 	metricMap = make(map[string]metricValue)
 	//metricStringMap = make(map[string]metric)
-	mI int64
+	lastCounterData int64
 )
 
 func int64ToBytes(value int64) [8]byte {
@@ -68,65 +68,68 @@ func check(err error) {
 func storeMetric(m map[string]metricValue) {
 	options := os.O_WRONLY | os.O_TRUNC | os.O_CREATE
 	file, err := os.OpenFile("metrics.data", options, os.FileMode(0600))
-	check(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	_, err = fmt.Fprintln(file, m)
-	check(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	err = file.Close()
-	check(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	q := r.URL.RequestURI()
-	log.Println(q)
+	//log.Println(q)
 	reqMethod := r.Method
-	log.Println(reqMethod)
-
-	//subpath := strings.Split(q, "/")
-	//fmt.Println(subpath)
-	//fmt.Println(reqMethod)
-	if reqMethod == "POST" {
-		var m1 metricValue
-		switch chi.URLParam(r, "metricType") {
-		case "gauge":
-			f, err := strconv.ParseFloat(chi.URLParam(r, "metricValue"), 64)
-			check(err)
-			m1.val = float64ToBytes(f)
-			fmt.Println(float64FromBytes(m1.val[:]))
-			m1.isCounter = false
-			metricMap[chi.URLParam(r, "metricName")] = m1
-			w.WriteHeader(http.StatusOK)
-			r.Body.Close()
-
-		case "counter":
-			i, err := strconv.ParseInt(chi.URLParam(r, "metricValue"), 10, 64)
-			check(err)
-			mI = mI + i
-			m1.val = int64ToBytes(mI)
-			m1.isCounter = true
-			fmt.Println(int64FromBytes(m1.val[:]))
-			metricMap[chi.URLParam(r, "metricName")] = m1
-			w.WriteHeader(http.StatusOK)
-			r.Body.Close()
-
-		default:
-			fmt.Println("Type", chi.URLParam(r, "metricType"), "wrong")
-			outputMessage := "Type " + chi.URLParam(r, "metricType") + " not supported, only [counter/gauge]"
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(outputMessage))
-			r.Body.Close()
-		}
-
-		//fmt.Println(metricMap)
-		storeMetric(metricMap)
-
-	} else {
-		fmt.Println("Method is wrong")
+	log.Println("New request with method " + reqMethod + ", uri: " + q)
+	if reqMethod != "POST" {
 		outputMessage := "Only POST method is alload"
+		log.Println("Wrong method for the handler. " + outputMessage)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(outputMessage))
-
 	}
+	var m metricValue
+	switch chi.URLParam(r, "metricType") {
+	case "gauge":
+		f, err := strconv.ParseFloat(chi.URLParam(r, "metricValue"), 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		m.val = float64ToBytes(f)
+		//log.Println(float64FromBytes(m.val[:]))
+		m.isCounter = false
+		metricMap[chi.URLParam(r, "metricName")] = m
+		w.WriteHeader(http.StatusOK)
+		r.Body.Close()
+
+	case "counter":
+		c, err := strconv.ParseInt(chi.URLParam(r, "metricValue"), 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		lastCounterData = lastCounterData + c // Change naming...
+		m.val = int64ToBytes(lastCounterData)
+		m.isCounter = true
+		//log.Println(int64FromBytes(m.val[:]))
+		metricMap[chi.URLParam(r, "metricName")] = m
+		w.WriteHeader(http.StatusOK)
+		r.Body.Close()
+
+	default:
+		log.Println("Type", chi.URLParam(r, "metricType"), "wrong")
+		outputMessage := "Type " + chi.URLParam(r, "metricType") + " not supported, only [counter/gauge]"
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(outputMessage))
+		r.Body.Close()
+	}
+
+	//log.Println(metricMap)
+	storeMetric(metricMap)
 }
 
 func handlerGetMetrics(w http.ResponseWriter, r *http.Request) {
@@ -136,69 +139,75 @@ func handlerGetMetrics(w http.ResponseWriter, r *http.Request) {
 	reqMethod := r.Method
 	log.Println(reqMethod)
 
-	if reqMethod == "GET" {
-		switch chi.URLParam(r, "metricType") {
-		case "gauge":
-			v, ok := metricMap[metricName]
+	if reqMethod != "GET" {
 
-			if !ok {
-				http.Error(w, "Not Found", http.StatusNotFound)
-				return
-			}
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintf(w, "%v\n", float64FromBytes(v.val[:]))
-
-			r.Body.Close()
-
-		case "counter":
-			v, ok := metricMap[metricName]
-
-			if !ok {
-				http.Error(w, "Not Found", http.StatusNotFound)
-				return
-			}
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintf(w, "%v\n", int64FromBytes(v.val[:]))
-
-			r.Body.Close()
-
-		default:
-			fmt.Println("Type", chi.URLParam(r, "metricType"), "wrong")
-			outputMessage := "Type " + chi.URLParam(r, "metricType") + " not supported, only [counter/gauge]"
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(outputMessage))
-			r.Body.Close()
-		}
-
-		fmt.Println(metricMap)
-		options := os.O_WRONLY | os.O_TRUNC | os.O_CREATE
-		file, err := os.OpenFile("metrics.data", options, os.FileMode(0600))
-		check(err)
-		_, err = fmt.Fprintln(file, metricMap)
-		check(err)
-		err = file.Close()
-		check(err)
-	} else {
-		fmt.Println("Method is wrong")
+		log.Println("Method is wrong")
 		outputMessage := "Only POST method is alload"
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(outputMessage))
 
 	}
+	switch chi.URLParam(r, "metricType") {
+	case "gauge":
+		v, ok := metricMap[metricName]
+
+		if !ok {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%v\n", float64FromBytes(v.val[:]))
+
+		r.Body.Close()
+
+	case "counter":
+		v, ok := metricMap[metricName]
+
+		if !ok {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%v\n", int64FromBytes(v.val[:]))
+
+		r.Body.Close()
+
+	default:
+		log.Println("Type", chi.URLParam(r, "metricType"), "wrong")
+		outputMessage := "Type " + chi.URLParam(r, "metricType") + " not supported, only [counter/gauge]"
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(outputMessage))
+		r.Body.Close()
+	}
+
+	log.Println(metricMap)
+	options := os.O_WRONLY | os.O_TRUNC | os.O_CREATE
+	file, err := os.OpenFile("metrics.data", options, os.FileMode(0600))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Fprintln(file, metricMap)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func httpPrintAllMetrics(w http.ResponseWriter, _ *http.Request) {
-	var mS1 metric
+	var stringMetricMap metric
 	metricStringMap := make(map[string]metric)
 	for k, v := range metricMap {
 		if !v.isCounter {
-			mS1.mtype = "gauge"
-			mS1.value = strconv.FormatFloat((float64FromBytes(v.val[:])), 'f', -1, 64)
-			metricStringMap[k] = mS1
+			stringMetricMap.mtype = "gauge"
+			stringMetricMap.value = strconv.FormatFloat((float64FromBytes(v.val[:])), 'f', -1, 64)
+			metricStringMap[k] = stringMetricMap
 		} else {
-			mS1.mtype = "counter"
-			mS1.value = strconv.FormatInt((int64FromBytes(v.val[:])), 10)
-			metricStringMap[k] = mS1
+			stringMetricMap.mtype = "counter"
+			stringMetricMap.value = strconv.FormatInt((int64FromBytes(v.val[:])), 10)
+			metricStringMap[k] = stringMetricMap
 		}
 
 	}
@@ -226,12 +235,15 @@ func main() {
 	r.Use(middleware.URLFormat)
 	r.Use(middleware.Timeout(60 * time.Second))
 	r.Post("/update/{metricType}/{metricName}/{metricValue}", handler)
+	r.Get("/update/{metricType}/{metricName}/{metricValue}", handler)
 	r.Get("/value/{metricType}/{metricName}", handlerGetMetrics)
 	r.Get("/", httpPrintAllMetrics)
 
 	// http.HandleFunc("/", handler)
 	// err := http.ListenAndServe("localhost:8080", nil)
-	fmt.Println("Serving on " + port)
+	log.Println("http server started on " + port)
 	err := http.ListenAndServe("127.0.0.1"+port, r)
-	check(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
